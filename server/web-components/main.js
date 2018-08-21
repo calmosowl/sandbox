ede44232defa9218f520bebd04a68c0a42bad05d
calmos.owl <calmos.owl@gmail.com>
2018-08-21 11:27:16 +0300

    Add row

--- server/web-components/main.js
+++ server/web-components/main.js
@@ -3,20 +3,6 @@ class GridTable extends HTMLElement {
   constructor() {
     super();
 
-    const source = [
-      { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-      { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-      { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-      { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-      { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-      { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-      { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-      { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-      { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-      { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-    ];
-
-    this.source = source;
     // Create a shadow root
     const shadow = this.attachShadow({ mode: 'open' });
 
@@ -24,8 +10,7 @@ class GridTable extends HTMLElement {
     const wrapper = document.createElement('div');
     wrapper.setAttribute('class', 'eui-table');
 
-    const row = document.createElement('div');
-    row.setAttribute('class', 'row');
+    
 
     // Create some CSS to apply to the shadow dom
     const style = document.createElement('style');
@@ -34,7 +19,11 @@ class GridTable extends HTMLElement {
       .eui-table {
         position: relative;
         display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(0, 1fr));
+        grid-template-rows: repeat(auto-fit, minmax(0, 1fr));
+      }
+      .row {
+      	display: grid;
+      	grid-template-columns: repeat(auto-fit, minmax(0, 1fr));
       }
       .col {
         color: #334;
@@ -48,46 +37,38 @@ class GridTable extends HTMLElement {
     // Attach the created elements to the shadow dom
     shadow.appendChild(style);
     shadow.appendChild(wrapper);
-    // wrapper.appendChild(row);
+
     this.wrapper = wrapper;
     
-		this.config = {
-			template: ['title', 'completed']
-		}
-
-		this.columns(this.config, this.wrapper);
-  }
-
-  createColumns() {
-    for (let i = 0; i < this.source.length; i++) {
-      for (let key in this.source[i]) {
-        const col = document.createElement('div');
-        col.setAttribute('class', `col ${key}`);
-        col.setAttribute('style', `grid-row: ${i + 1}`);
-        col.textContent = this.source[i][key];
-        this.wrapper.appendChild(col);
-      }
+	this.config = {
+      template: ['title', 'completed']
     }
+
+	this.columns(this.config, this.wrapper);
   }
 
-  columns(config, wrapper){
-  fetch('https://jsonplaceholder.typicode.com/todos')
-    .then(response => response.json())
-    .then(function(data) {
-		let row = 0;
-		while(row < data.length) {
-	  	for(let key of config.template) {
-  		  const col = document.createElement('div');
-  		  
-  		  col.setAttribute('class','col');
-  		  col.setAttribute('style', `grid-row: ${row + 1}`);
-  		  
-  		  col.textContent=data[row][key];
- 		  	wrapper.appendChild(col); 		  
- 			 }
- 		  row++;
- 		  }
- 		});
+  columns(config, wrapper) {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(function(data) {
+		let i = 0;
+		while(i < data.length) {
+		  const row = document.createElement('div');
+		  row.setAttribute('class', 'row');
+		  row.setAttribute('style', `grid-row: ${i + 1}`);
+
+          for(let key of config.template) {
+  		    const col = document.createElement('div');
+  		    col.setAttribute('class',`col ${key}`);
+  		    col.textContent=data[i][key];
+
+            row.appendChild(col); 		  
+          }
+          
+ 	      wrapper.appendChild(row); 		  
+ 		  i++;
+        }
+    });
   }
 }
 
